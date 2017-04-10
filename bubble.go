50f718cf4f0e1b16343d1036ad6ebd88c76c33c9
yiğit oztemel <yigit.oztemel@beyn.com.tr>
2017-04-10 12:18:57 +0300

    firt commit

---
+++ bubble.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+// process run count
+var runCount int
+
+func main() {
+	var arr []int
+
+	//best case
+	arr = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	//worst case
+	arr = []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+
+	fmt.Println(runCount, bubbleSort(arr))
+
+}
+
+func bubbleSort(arr []int) []int {
+
+	l := len(arr)
+	var changed bool
+
+	for i := 0; i < l-1; i++ {
+		changed = false
+		for j := 0; j < l-1; j++ {
+			runCount++
+			if arr[j] > arr[j+1] {
+				changed = true
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+		if changed == false {
+			break
+		}
+	}
+	return arr
+}
